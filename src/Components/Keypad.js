f3344fb879f47e79a0061e253e75e1fc71e1ef67
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-26 16:15:18 +0200

    Converted component to functional component

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Number from './Number';
 import Operator from './Operator';
 import Equals from './Equals';
 import Clear from './Clear';
 
-class Keypad extends Component {
-  render() {
-    let numbers = [...Array(10).keys()].map(i => (
-      <Number
-        number={i}
-        key={i}
-        onNumberClick={i => this.props.onNumberClick(i)}
-      />
-    ));
-    let operators = this.props.operators.map(i => (
-      <Operator
-        operator={i}
-        key={i}
-        onOperatorClick={i => this.props.onOperatorClick(i)}
-      />
-    ));
+const Keypad = props => {
+  let numbers = [...Array(10).keys()].map(i => (
+    <Number number={i} key={i} onNumberClick={i => props.onNumberClick(i)} />
+  ));
+  let operators = props.operators.map(i => (
+    <Operator
+      operator={i}
+      key={i}
+      onOperatorClick={i => props.onOperatorClick(i)}
+    />
+  ));
 
-    return (
-      <div className="Keypad">
-        {numbers}
-        {operators}
-        <Equals onEqualsClick={() => this.props.onEqualsClick()} />
-        <Clear onClearClick={() => this.props.onClearClick()} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Keypad">
+      {numbers}
+      {operators}
+      <Equals onEqualsClick={() => props.onEqualsClick()} />
+      <Clear onClearClick={() => props.onClearClick()} />
+    </div>
+  );
+};
 
 export default Keypad;
