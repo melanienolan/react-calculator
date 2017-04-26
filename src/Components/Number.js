8ddbdb491beed116e3f82e8562a3298df72f53c7
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-26 16:15:56 +0200

    Converted component to functional component

--- src/Components/Number.js
+++ src/Components/Number.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Number extends Component {
-  render() {
-    return (
-      <div
-        onClick={() => this.props.onNumberClick(this.props.number)}
-        className="Number"
-      >
-        {this.props.number}
-      </div>
-    );
-  }
-}
+const Number = props => {
+  return (
+    <div onClick={() => props.onNumberClick(props.number)} className="Number">
+      {props.number}
+    </div>
+  );
+};
 
 export default Number;
