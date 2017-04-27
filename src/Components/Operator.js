677d2050b6c2e606b0cd0696e78d6ddc14a85fc6
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-27 11:57:01 +0200

    added styles for operator buttons

--- src/Components/Operator.js
+++ src/Components/Operator.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const Operator = props => {
+  const operatorStyles = {
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+    margin: '2px',
+    backgroundColor: 'honeydew',
+    fontSize: '20px'
+  };
   return (
     <div
-      className="Operator"
+      style={operatorStyles}
       onClick={() => props.onOperatorClick(props.operator)}
     >
       {props.operator}
