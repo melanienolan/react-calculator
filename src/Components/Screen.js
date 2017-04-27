d9f90083405b9856251021bbb53c8a6aa4f4e7ec
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-27 11:57:50 +0200

    added styles for screen component

--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Screen = props => {
-  return <div className="Screen">{props.display}</div>;
+  const screenStyles = {
+    height: '60px',
+    width: '90%',
+    border: '1px solid grey',
+    borderRadius: '10px',
+    backgroundColor: 'whitesmoke',
+    padding: '10px',
+    textAlign: 'right',
+    fontSize: '30px',
+    lineHeight: '50px',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap'
+  };
+  return <div style={screenStyles}>{props.display}</div>;
 };
 
 export default Screen;
