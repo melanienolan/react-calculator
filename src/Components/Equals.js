122a2ca1865baa9c7d4495095501fe34cc169d92
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-27 11:53:37 +0200

    added styles for equals button

--- src/Components/Equals.js
+++ src/Components/Equals.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Equals = props => {
+  const equalsStyles = {
+    width: '104px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+    margin: '2px',
+    backgroundColor: 'honeydew',
+    fontSize: '20px'
+  };
   return (
-    <div className="Equals" onClick={() => props.onEqualsClick()}>
+    <div style={equalsStyles} onClick={() => props.onEqualsClick()}>
       =
     </div>
   );
