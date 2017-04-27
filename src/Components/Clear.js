594de5941953c06a8730bf58bc9216a290ff21fc
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-27 11:51:42 +0200

    Added styles for clear component

--- src/Components/Clear.js
+++ src/Components/Clear.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Clear = props => {
+  const clearStyles = {
+    width: '90%',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+    backgroundColor: 'plum',
+    fontSize: '20px',
+    color: 'white'
+  };
   return (
-    <div className="Clear" onClick={() => props.onClearClick()}>
+    <div style={clearStyles} onClick={() => props.onClearClick()}>
       AC
     </div>
   );
