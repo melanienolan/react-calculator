f739ee1e37dcc498b66d9a09da36e1a7e96a078a
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-04-27 11:56:18 +0200

    added styles for number buttons

--- src/Components/Number.js
+++ src/Components/Number.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Number = props => {
+  const numberStyles = {
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+    margin: '2px',
+    backgroundColor: 'honeydew',
+    fontSize: '20px'
+  };
   return (
-    <div onClick={() => props.onNumberClick(props.number)} className="Number">
+    <div style={numberStyles} onClick={() => props.onNumberClick(props.number)}>
       {props.number}
     </div>
   );
