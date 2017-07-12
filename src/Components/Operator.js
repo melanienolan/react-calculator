f74df6f3306a06083ba8a156395933434689a5e1
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-12 17:24:42 +0200

    added prop types for operator component

--- src/Components/Operator.js
+++ src/Components/Operator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Operator = props => {
   const operatorStyles = {
@@ -13,13 +14,15 @@ const Operator = props => {
     fontSize: '20px'
   };
   return (
-    <div
-      style={operatorStyles}
-      onClick={() => props.onOperatorClick(props.operator)}
-    >
+    <div style={operatorStyles} onClick={() => props.onOperatorClick(props.operator)}>
       {props.operator}
     </div>
   );
+};
+
+Operator.propTypes = {
+  operator: PropTypes.string,
+  onOperatorClick: PropTypes.func
 };
 
 export default Operator;
