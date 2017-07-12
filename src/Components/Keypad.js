4c0ef2d7a07d108e5d22d1099decbf32acc1e35b
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-07-12 17:13:23 +0200

    added prop types for keypad component

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Number from './Number';
 import Operator from './Operator';
 import Equals from './Equals';
@@ -25,16 +26,12 @@ const Keypad = props => {
   let numbers = [...Array(10).keys()];
   let shifted = numbers.shift();
   numbers.push(shifted);
-  numbers = numbers.map(i => (
+  numbers = numbers.map(i =>
     <Number number={i} key={i} onNumberClick={i => props.onNumberClick(i)} />
-  ));
-  let operators = props.operators.map(i => (
-    <Operator
-      operator={i}
-      key={i}
-      onOperatorClick={i => props.onOperatorClick(i)}
-    />
-  ));
+  );
+  let operators = props.operators.map(i =>
+    <Operator operator={i} key={i} onOperatorClick={i => props.onOperatorClick(i)} />
+  );
 
   return (
     <div style={keypadStyles}>
@@ -43,10 +40,17 @@ const Keypad = props => {
         <Decimal onDecimalClick={props.onDecimalClick} />
         <Equals onEqualsClick={props.onEqualsClick} />
       </div>
-      <div style={oHolderStyles}>{operators}</div>
-
+      <div style={oHolderStyles}>
+        {operators}
+      </div>
     </div>
   );
+};
+
+Keypad.propTypes = {
+  onEqualsClick: PropTypes.func,
+  onDecimalClick: PropTypes.func,
+  operators: PropTypes.array
 };
 
 export default Keypad;
